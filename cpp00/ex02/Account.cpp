1af0d76bb6e21ac9bf1a1064f444672b8f0415ac
rsudo <badbad7351@gmail.com>
2021-11-09 12:47:23 +0900

    update ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,10 +1,10 @@
 #include "Account.hpp"
 #include <iostream>
 
-int Account::_nbAccounts = 0;
-int Account::_totalAmount = 0;
-int Account::_totalNbDeposits = 0;
-int Account::_totalNbWithdrawals = 0;
+/* int Account::_nbAccounts = 0; */
+/* int Account::_totalAmount = 0; */
+/* int Account::_totalNbDeposits = 0; */
+/* int Account::_totalNbWithdrawals = 0; */
 
 Account::Account(int initial_deposit):
 	_accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
@@ -23,6 +23,9 @@ Account::~Account(void)
 	std::cout << "index:" << _accountIndex << ";"
 		<< "amount:" << _amount << ";"
 		<< "closed" << std::endl;
+
+	_nbAccounts--;
+	_totalAmount -= _amount;
 }
 
 int	Account::getNbAccounts( void )
