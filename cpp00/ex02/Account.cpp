d6afb284c342ed5dddf606cd8534e5860c20cf9e
rsudo <badbad7351@gmail.com>
2021-11-09 12:59:47 +0900

    update Account.cpp

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,10 +1,10 @@
 #include "Account.hpp"
 #include <iostream>
 
-/* int Account::_nbAccounts = 0; */
-/* int Account::_totalAmount = 0; */
-/* int Account::_totalNbDeposits = 0; */
-/* int Account::_totalNbWithdrawals = 0; */
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit):
 	_accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
@@ -115,4 +115,3 @@ void	Account::_displayTimestamp( void )
 {
 	std::cout << "[19920104_091532] ";
 }
-
