16f01df8d5b8c6aa0bbcbe4c9548ae002f33ed8a
rsudo <badbad7351@gmail.com>
2022-01-22 10:49:40 +0900

    cpp07/ex00 clnag-format

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
+
 #include "whatever.hpp"
 
-int main( void ) {
-	int a = 2;
-	int b = 3;
+int main(void) {
+    {
+        int a = 2;
+        int b = 3;
 
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+        ::swap(a, b);
+        std::cout << "a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+        std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	}
+	{
+        std::string c = "chaine1";
+        std::string d = "chaine2";
 
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+        ::swap(c, d);
+        std::cout << "c = " << c << ", d = " << d << std::endl;
+        std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+        std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	}
+	{
+		const char e = 'a';
+		const char f = 'b';
+		const char min = ::min(e, f);
+		std::cout << "e = " << e << ", f = " << f << std::endl;
+		std::cout << "min( e, f) = " << min << std::endl;
+		std::cout << "max( e, f) = " << ::max(e, f) << std::endl;
+	}
 
-	return 0;
+    return 0;
 }
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -1,27 +1,27 @@
-template<typename T>
-T max(T a, T b)
-{
-	if (a < b)
-	{
-		return b;
-	}
-	return a;
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+// const??
+template <typename T>
+T max(T a, T b) {
+    if (a < b) {
+        return b;
+    }
+    return a;
 }
 
-template<typename T>
-T min(T a, T b)
-{
-	if (a < b)
-	{
-		return a;
-	}
-	return b;
+template <typename T>
+const T min(const T a, const T b) {
+    if (a < b) {
+        return a;
+    }
+    return b;
 }
 
-template<typename T>
-void swap(T &a, T &b)
-{
-	T tmp = a;
-	a = b;
-	b = tmp;
+template <typename T>
+void swap(T &a, T &b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
 }
+#endif  // WHATEVER_HPP
