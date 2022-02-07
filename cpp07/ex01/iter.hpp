233a85d825f7036e0d33e58881a2a69087f86450
rsudo <badbad7351@gmail.com>
2022-02-07 17:41:04 +0900

    cpp07/ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,17 +1,18 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
 #include <iostream>
 
-template<typename T>
-void write(T a)
-{
-	std::cout << a << std::endl;
+template <typename T>
+void write(T a) {
+    std::cout << a << std::endl;
 }
 
-
-template<typename T>
-void iter(T *arr, std::size_t len, void (*func)(T))
-{
-	for (std::size_t i = 0; i < len; i++)
-	{
-		func(arr[i]);
-	}
+template <typename T>
+void iter(T arr[], std::size_t len, void (*func)(T)) {
+    for (std::size_t i = 0; i < len; i++) {
+        func(arr[i]);
+    }
 }
+
+#endif  // ITER_HPP
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
-#include "iter.hpp"
 #include <string>
 
-int main()
-{
-	{
-		std::cout << "--- test1 ---" << std::endl;
-		char arr[] = {'a', 'b', 'c', 'd'};
-		iter(arr, 3, &write);
-	}
+#include "iter.hpp"
 
-	{
-		std::cout << "\n--- test2 ---" << std::endl;
-		int arr[] = {1, 2, 3, 4, 5, 6};
-		iter(arr, 6, &write);
-	}
+int main() {
+    {
+        std::cout << "--- char ---" << std::endl;
+        char arr[] = {'a', 'b', 'c', 'd'};
+        iter(arr, 3, &write);
+    }
 
-	{
-		std::cout << "\n--- test3 ---" << std::endl;
-		std::string arr[] = {"hgoe", "fuga"};
-		iter(arr, 5, &write);
-	}
+    {
+        std::cout << "\n--- int ---" << std::endl;
+        int arr[] = {1, 2, 3, 4, 5, 6};
+        iter(arr, 6, &write);
+    }
+
+    {
+        std::cout << "\n--- string ---" << std::endl;
+        std::string arr[] = {"hgoe", "hogeee"};
+        iter(arr, 2, &write);
+    }
 }
-
