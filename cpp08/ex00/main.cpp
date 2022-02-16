4aa0d18b37b2bdc88251706058aaa612a2b30c42
rsudo <badbad7351@gmail.com>
2022-02-16 14:38:23 +0900

    cpp08/ex00 add comment

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -18,30 +18,32 @@ void vecterTest() {
     for (std::size_t i = 1; i < size; i++) {
         intVec.push_back(i);
     }
+
     for (std::size_t i = 0; i < intVec.size(); i++) {
         std::cout << "[" << i << "]: " << intVec[i] << "  ";
     }
+
     std::cout << std::endl;
-    testEasyfind(intVec, 0);
-    testEasyfind(intVec, 1);
-    testEasyfind(intVec, 3);
-    testEasyfind(intVec, 9);
-    testEasyfind(intVec, 10);
+    testEasyfind(intVec, 0);   // will not find
+    testEasyfind(intVec, 1);   // will find
+    testEasyfind(intVec, 3);   // will find
+    testEasyfind(intVec, 9);   // will find
+    testEasyfind(intVec, 10);  // will not find
 }
 
 void setTest() {
-    std::cout << GREEN << "\n--- setTest ---" << RESET << std::endl;
+    std::cout << GREEN << "--- setTest ---" << RESET << std::endl;
     std::set<int> intSet;
 
     intSet.insert(-100);
     intSet.insert(500);
     intSet.insert(INT_MAX);
-    testEasyfind(intSet, 0);
-    testEasyfind(intSet, -100);
+
+    testEasyfind(intSet, 0);     // will not find
+    testEasyfind(intSet, -100);  // will find
 }
 
 int main() {
     vecterTest();
     setTest();
-    std::multiset<int> intset;
 }
